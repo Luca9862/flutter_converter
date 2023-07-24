efe2dbdffd557ae2f6a0387cb051f2a73a9f1607
luca.canali <lucacanali.dev@gmail.com>
2023-07-24 22:54:07 +0200

    Update and fix binary converter

--- lib/binaryscreen.dart
+++ lib/binaryscreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'navigationbar.dart';
+import 'package:flutter/services.dart';
 
 class BinaryScreen extends StatefulWidget {
   const BinaryScreen({super.key});
@@ -10,9 +9,6 @@ class BinaryScreen extends StatefulWidget {
 }
 
 class _BinaryScreen extends State<BinaryScreen> {
-
-  String binary = '';
-
   final myController = TextEditingController();
 
   @override
@@ -21,25 +17,38 @@ class _BinaryScreen extends State<BinaryScreen> {
     super.dispose();
   }
 
-  toBinary(int n){
+  String toBinary(int n) {
     if (n == 0) {
-      return '0';
+      return '00000000';
     }
+    String binary = '';
     while (n > 0) {
       binary = (n % 2).toString() + binary;
       n ~/= 2;
     }
-    return binary;
-  }
-
-  String stringToBinary(String s) {
-    String binary = '';
-    for (int i = 0; i < s.length; i++) {
-      binary += toBinary(s.codeUnitAt(i)) + ' ';
+    // Aggiungi zeri iniziali (padding) fino a raggiungere 8 bit
+    while (binary.length < 8) {
+      binary = '0$binary';
     }
     return binary;
   }
 
+  stringToBinary(String s) {
+    String binary = '';
+    for (int i = 0; i < s.length; i++) {
+      int charValue = s.codeUnitAt(i); // Ottieni il valore ASCII del carattere
+      binary += "${toBinary(charValue)} ";
+    }
+    return binary;
+  }
+
+  void copyToClipboard() {
+    String binaryText = stringToBinary(myController.text);
+    Clipboard.setData(ClipboardData(text: binaryText));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Testo copiato negli appunti!')),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,30 +57,34 @@ class _BinaryScreen extends State<BinaryScreen> {
         title: const Text('Binary convert'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: TextField(
-              controller: myController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Insert',
-              ),
-            )
-          ),
-          Text(
-            'Result: $binary',
-            style: const TextStyle(fontSize: 24),
-          ),
-        ]
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: TextField(
+                  controller: myController,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Insert',
+                  ),
+                )
+            ),
+            const SizedBox(height: 16),
+            SelectableText(
+              'Result: ${stringToBinary(myController.text)}', // Chiamiamo stringToBinary direttamente qui
+              style: const TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () => copyToClipboard(),
+              child: const Text('Copia'),
+            ),
+          ]
       ),
       //bottomNavigationBar: const MyBottomNavigationBar(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            binary = '';
-            stringToBinary(myController.text);
+            // Nessun bisogno di resettare la variabile binary qui
           });
         },
         child: const Icon(
